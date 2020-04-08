36f205f7ccef0ef701b93d5bff68f8cdf429d2d0
narutoalai <31005849+narutolai@users.noreply.github.com>
2020-04-08 14:40:53 +0800

    Create 二叉树搜索范围和.cpp

---
+++ 和树有关/二叉树搜索范围和.cpp
@@ -0,0 +1,29 @@
+//给的一颗二叉树，返回L和R范围之间的数的和 包括其本身。
+//其实就是先序遍历了
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    int res;
+public:
+    void preorder(TreeNode *root,int l,int r)
+    {
+        if(!root) return;
+        if(r>=root->val&&root->val>=l)  //判断是否在范围内
+            res+=root->val;
+        preorder(root->left,l,r);
+        preorder(root->right,l,r);
+    }
+    int rangeSumBST(TreeNode* root, int L, int R) {
+            preorder(root,L,R);
+            return res;
+
+
+    }
+};
