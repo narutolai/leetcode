6c3089a9b41e889d75dfe5793155ab01968f1d22
narutoalai <31005849+narutolai@users.noreply.github.com>
2020-04-08 15:12:10 +0800

    Create 二叉搜索树中的查找.cpp

---
+++ 和树有关/二叉搜索树中的查找.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+    /*  递归方式*/
+        if(root==NULL||root->val==val) return root;
+        return val<root->val?searchBST(root->left,val):searchBST(root->right,val);
+        //迭代方式
+/*
+        while(root!=NULL&&root->val!=val)
+        {
+            root=root->val>val?root->left:root->right;
+        }
+        return root;*/
+    }
