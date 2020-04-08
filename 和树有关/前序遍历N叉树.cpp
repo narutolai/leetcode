8f1028be3cc992df8b149715d9a3338f5fb8efb3
narutoalai <31005849+narutolai@users.noreply.github.com>
2020-04-08 15:19:00 +0800

    Create 前序遍历N叉树.cpp

---
+++ 和树有关/前序遍历N叉树.cpp
@@ -0,0 +1,56 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    //  vector<int> result;
+public:
+    /*void dfs(Node *root){
+        if(!root) return;
+        result.push_back(root->val);
+            for(auto p:root->children)
+            {   
+                preorder(p);
+            }
+    }*/
+    vector<int> preorder(Node* root) {
+      /*  dfs(root);
+        return result;
+        */
+    //迭代方式
+    if(!root) return {};
+    stack<Node*> st;
+    Node *cur;
+    vector<int> result;
+    st.push(root);
+
+    while(!st.empty())
+    {
+         cur=st.top();          //取栈顶
+         st.pop();               //弹出元素
+         result.push_back(cur->val);
+         reverse(cur->children.begin(),cur->children.end()) ; //
+         for(auto p:cur->children)//入栈
+         {
+                st.push(p);
+         }
+    }
+return result;
+
+    }
+};
