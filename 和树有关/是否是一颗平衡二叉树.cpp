c44ae6d9a469894eeb5a39eba837926e0f5d0378
narutoalai <31005849+narutolai@users.noreply.github.com>
2020-04-08 17:05:45 +0800

    Create 是否是一颗平衡二叉树.cpp

---
+++ 和树有关/是否是一颗平衡二叉树.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ 
+ //可以用二叉树的深度来解答
+class Solution {
+    bool flag;
+public:
+    int dfs(TreeNode *root)
+    {
+        if(!root) return 0;
+        int leftdepth=dfs(root->left);
+        int rightdepth=dfs(root->right);  
+        if(abs(leftdepth-rightdepth)>1)   //如果左右子树深度之差大于1则返回false
+                flag=false;
+        return max(leftdepth,rightdepth)+1;
+        
+    }
+    
+    bool isBalanced(TreeNode* root) {
+            flag=true;
+            dfs(root);
+            return flag;
+
+    }
+};
