c277d838f472d03766689d3a4e1d72184710e1f9
narutoalai <31005849+narutolai@users.noreply.github.com>
2020-04-09 17:20:54 +0800

    Create 最长回文子串.cpp

---
+++ 动态规划-中等/最长回文子串.cpp
@@ -0,0 +1,61 @@
+//首先给出java版本的中心算法
+public String longestPalindrome(String s) {
+    if (s == null || s.length() < 1) return "";
+    int start = 0, end = 0;
+    for (int i = 0; i < s.length(); i++) {
+        int len1 = expandAroundCenter(s, i, i); //因为不知道长度是奇数还是偶数
+        int len2 = expandAroundCenter(s, i, i + 1);
+        int len = Math.max(len1, len2);
+        if (len > end - start) {
+            start = i - (len - 1) / 2;
+            end = i + len / 2;
+        }
+    }
+    return s.substring(start, end + 1);
+}
+private int expandAroundCenter(String s, int left, int right) {
+    int L = left, R = right;
+    while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+        L--;
+        R++;
+    }
+    return R - L - 1;
+}
+
+//然后下面是c++的动态规划版本的
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int len=s.size();
+        if(len==0||len==1)
+            return s;
+        int start=0;//回文串起始位置
+        int max=1;//回文串最大长度
+        vector<vector<int>>  dp(len,vector<int>(len));//定义二维动态数组
+        for(int i=0;i<len;i++)//初始化状态
+        {
+            dp[i][i]=1;
+            if(i<len-1&&s[i]==s[i+1])
+            {
+                dp[i][i+1]=1;
+                max=2;
+                start=i;
+            }
+        }
+        for(int l=3;l<=len;l++)//l表示检索的子串长度，等于3表示先检索长度为3的子串
+        {
+            for(int i=0;i+l-1<len;i++)
+            {
+                int j=i+l-1;//终止字符位置
+                if(s[i]==s[j]&&dp[i+1][j-1]==1)//状态转移
+                {
+                    dp[i][j]=1;
+                    start=i;
+                    max=l;
+                }
+            }
+        }
+        return s.substr(start,max);//获取最长回文子串
+    }
+};
+
