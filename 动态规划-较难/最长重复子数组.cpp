e8e76cfd94e3bec2943028c70cf0b13a73f94c32
narutoalai <31005849+narutolai@users.noreply.github.com>
2020-04-14 23:09:19 +0800

    子数组连续 子序列不连续

---
+++ 动态规划-较难/最长重复子数组.cpp
@@ -0,0 +1,36 @@
+给两个整数数组 A 和 B ，返回两个数组中公共的、长度最长的子数组的长度。
+
+示例 1:
+
+输入:
+A: [1,2,3,2,1]
+B: [3,2,1,4,7]
+输出: 3
+解释: 
+长度最长的公共子数组是 [3, 2, 1]。
+说明:
+
+1 <= len(A), len(B) <= 1000
+0 <= A[i], B[i] < 100
+
+
+
+class Solution {
+public:
+//这算是KMP字符串匹配吗
+    int findLength(vector<int>& A, vector<int>& B) {
+            int m=A.size(),n=B.size();
+            vector<vector<int>> dp(m,vector<int>(n,0));
+            int len=0;
+            for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+            {
+                if(A[i]==B[j])
+                {
+                dp[i][j]=((i-1<0||j-1<0)?0:dp[i-1][j-1])+1;
+                len=max(len,dp[i][j]);
+                }
+            }
+            return len;
+    }
+};
