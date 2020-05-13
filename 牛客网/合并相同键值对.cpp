fc2345d84f1c6fe682aeca7c031df0293a6c1983
narutoalai <31005849+narutolai@users.noreply.github.com>
2020-05-13 09:42:02 +0800

    Create 合并相同键值对.cpp

---
+++ 牛客网/合并相同键值对.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main()
+{
+    map<int,int> kk;
+    int m;
+    while(cin>>m)
+    {
+        int index,value;
+        while(m)
+        {
+        cin>>index>>value;
+        kk[index]+=value;
+            m--;
+        }
+    map<int,int>::iterator iter;
+    for(iter=kk.begin();iter!=kk.end();iter++)
+        cout<<iter->first<<" "<<iter->second<<endl;
+    }
+    return 0;
+}
