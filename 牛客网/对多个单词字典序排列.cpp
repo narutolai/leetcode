765bad9605450c7d45d06f9041dcd89bea64bdf0
narutoalai <31005849+narutolai@users.noreply.github.com>
2020-05-13 12:01:38 +0800

    Create 对多个单词字典序排列.cpp

---
+++ 牛客网/对多个单词字典序排列.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <algorithm>
+using namespace std;
+ 
+bool compare(const string &s1, const string &s2) {
+    return s1<s2;
+}
+ 
+int main(){
+    int n;
+    vector<string> result;
+    while(cin >> n){
+        while(n!=0){
+            n--;
+            string t;
+            cin >> t;
+            result.push_back(t);
+        }
+        sort(result.begin(),result.end(),compare);
+        for(int i = 0;i<result.size();i++){
+            cout << result[i] << endl;
+        }
+    }
+}
