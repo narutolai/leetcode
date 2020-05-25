7bfb38103fb050daf3cd429641567689432c8d93
narutoalai <31005849+narutolai@users.noreply.github.com>
2020-05-25 14:47:22 +0800

    Create 青蛙跳台阶.cpp

---
+++ 剑指offer/青蛙跳台阶.cpp
@@ -0,0 +1,16 @@
+青蛙可以一次跳一次或一次跳两次
+class Solution {
+public:
+    int numWays(int n) {
+        int pre=1,cur=1;
+        if(n==0||n==1) return 1;
+        int mode=1e9+7;
+        for(int i=2;i<=n;i++)
+        {
+            int temp=pre%mode+cur%mode;
+            pre=cur;
+            cur=temp;
+        }
+        return cur%mode;
+    }
+};
