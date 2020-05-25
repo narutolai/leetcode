b18273f7704bd9fc43b7f943b640a8078fda3e15
narutoalai <31005849+narutolai@users.noreply.github.com>
2020-05-25 16:56:07 +0800

    Create 删除单向链表的一个节点.cpp

---
+++ 剑指offer/删除单向链表的一个节点.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ //这个head是第一个节点不是头节点。 不带头节点的。
+class Solution {
+public:
+    ListNode* deleteNode(ListNode* head, int val) {
+        //初始化的时候就要定义当前和下一个。
+        ListNode *cur=head;
+        ListNode *next=cur->next;
+        if(cur->val==val) return next;
+        
+        while(next!=NULL)
+       {
+           if(next->val==val)
+           {
+               cur->next=next->next;
+           }
+           cur=next;
+           next=next->next;
+       }
+        return head;
+    }
+};
