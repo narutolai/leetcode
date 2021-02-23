53dd58bc74da04017e0cf66c7ba72e3502f75e8d
narutoalai <31005849+narutolai@users.noreply.github.com>
2021-02-23 12:12:42 +0800

    Create 566重塑数组.cpp

---
+++ 剑指offer/566重塑数组.cpp
@@ -0,0 +1,20 @@
+就是python的reshape()函数
+
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        int m=nums.size();
+        int n=nums[0].size();
+        if(m*n!=r*c) return nums;
+
+        vector<vector<int>> numsnew(r,vector<int>(c,0));
+
+        int i=0;
+        while(i<m*n)
+        {
+            numsnew[i/c][i%c]=nums[i/n][i%n];
+            i++;
+        }
+        return numsnew;
+    }
+};
