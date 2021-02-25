ba4cd9196f113b4419103737cb1b5d4d114108ee
narutoalai <31005849+narutolai@users.noreply.github.com>
2021-02-25 11:27:08 +0800

    Create 矩阵装置.cpp

---
+++ 剑指offer/矩阵装置.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<vector<int>> newmatrix(n,vector<int>(m,0));
+        for(int i=0;i<matrix.size();i++)
+        for(int j=0;j<matrix[0].size();j++)
+            newmatrix[j][i]=matrix[i][j];
+        return newmatrix;
+    }
+};
+
