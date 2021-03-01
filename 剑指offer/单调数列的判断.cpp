eb8ad06ccfe548a2dc498a3a132889fbab33e248
narutoalai <31005849+narutolai@users.noreply.github.com>
2021-03-01 14:54:06 +0800

    Create 单调数列的判断.cpp

---
+++ 剑指offer/单调数列的判断.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& A) {
+            int up=0,down=0;
+            for(int i(0);i<A.size()-1;i++)
+            {
+                if(A[i]<A[i+1])
+                {
+                    up = 1;
+                    if(down)
+                    {
+                        return false;
+                    }
+                }else if(A[i]>A[i+1])
+                {
+                    down=1;
+                    if(up)
+                    {
+                        return false;
+                    }
+                }
+                else{continue;}
+            }
+            return true;
+    }
+};
+一次遍历  还可以提前退出
