fcca4621a8fb1e2b0ee54569da2cc733871b9ded
narutoalai <31005849+narutolai@users.noreply.github.com>
2021-03-09 14:33:53 +0800

    Create 删除字符串中的重复字符.cpp

---
+++ 剑指offer/删除字符串中的重复字符.cpp
@@ -0,0 +1,56 @@
+给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
+
+在 S 上反复执行重复项删除操作，直到无法继续删除。
+
+在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
+
+ 
+
+示例：
+
+输入："abbaca"
+输出："ca"
+解释：
+例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+
+
+class Solution {
+public:
+
+void dfs(string &s,int index)
+{
+	if(index==s.size()||s.size()==1)
+	{
+		return ;
+	}
+	for(int i=index;i<int(s.size())-1;i++)
+	{
+		if(s[i]==s[i+1])
+		{
+			s.erase(i,2);
+			dfs(s,i-1<0?0:i-1);
+		}
+	}
+
+}
+    string removeDuplicates(string S) {
+        dfs(S,0);
+        return S;
+    }
+};
+
+
+class Solution {
+public:
+    string removeDuplicates(string S) {
+        string stk;
+        for (char ch : S) {
+            if (!stk.empty() && stk.back() == ch) {
+                stk.pop_back();
+            } else {
+                stk.push_back(ch);
+            }
+        }
+        return stk;
+    }
+};
