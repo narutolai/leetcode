0fca19873ddfc87fa4278e28e77224318cc6656d
narutoalai <31005849+narutolai@users.noreply.github.com>
2021-03-22 14:14:03 +0800

    Create 逆波兰表达式.cpp

---
+++ 2021322/逆波兰表达式.cpp
@@ -0,0 +1,66 @@
+根据 逆波兰表示法，求表达式的值。
+
+有效的算符包括 +、-、*、/ 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+
+ 
+
+说明：
+
+整数除法只保留整数部分。
+给定逆波兰表达式总是有效的。换句话说，表达式总会得出有效数值且不存在除数为 0 的情况。
+ 
+
+示例 1：
+
+输入：tokens = ["2","1","+","3","*"]
+输出：9
+解释：该算式转化为常见的中缀算术表达式为：((2 + 1) * 3) = 9
+  
+class Solution
+{
+public:
+    int evalRPN(vector<string> &tokens)
+    {
+        stack<int> st;
+        for (auto &it : tokens)
+        {
+            if (it.back() >= '0' && it.back() <= '9')
+            {
+                st.push(atoi(it.c_str()));
+            }
+            else
+            {
+                int  m = st.top();
+                    st.pop();
+                 int   n = st.top();
+                    st.pop();
+                switch (*it.c_str()) //这里返回的是一个指针，指向字符，所以只能取内容了
+                {
+                case '+':
+                {
+                    st.push(m + n);
+                }
+                break;
+                case '-':
+                {
+                    st.push(n - m);
+                }
+                break;
+                case '*':
+                {
+                    st.push(m * n);
+                }
+                break;
+                case '/':
+                {
+                    st.push(n / m);
+                }
+                break;
+                default:
+                    break;
+                }
+            }
+        }
+        return st.top();
+    }
+};
