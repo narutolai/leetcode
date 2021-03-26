488b1d33c8385722bf5dfd2c39dfb8cef3e7ef96
narutoalai <31005849+narutolai@users.noreply.github.com>
2021-03-26 10:17:06 +0800

    Create 删除有序链表中的重复元素.cpp

---
+++ 2021322/删除有序链表中的重复元素.cpp
@@ -0,0 +1,40 @@
+删除有序链表中的重复元素，使得其只出现一次
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *cur = head;
+        ListNode *pre = nullptr;
+        head=nullptr;
+        while (cur != nullptr)
+        {
+            if (pre==nullptr?1:pre->val!=cur->val)
+            {
+                if (pre == nullptr)
+                {
+                    pre = cur;
+                    head = cur;
+                }
+                else
+                {
+                    pre->next = cur;
+                    pre = cur;
+                }
+            }
+            cur = cur->next;
+        }
+        if(pre!=nullptr)
+            pre->next=nullptr;
+        return head;
+    }
+};
