cb745d5fb747f6ca8d335a84d460c3c12f969042
narutoalai <31005849+narutolai@users.noreply.github.com>
2021-03-29 19:38:47 +0800

    Create 高效搜索素数.cpp

---
+++ 2021322/高效搜索素数.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<int> isPrime(n, 1);
+        int ans = 0;
+        for (int i = 2; i < n; ++i) {
+            if (isPrime[i]) {
+                ans += 1;
+                if ((long long)i * i < n) {
+                    for (int j = i * i; j < n; j += i) {
+                        isPrime[j] = 0;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+为什么j要从i*i开始呢， 因为 i*（i-1） 会被 j=i-1的时候 给计算掉的，
