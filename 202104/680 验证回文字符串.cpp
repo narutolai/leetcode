6b8fabd3657a705ae7ca29e56e23c770ae9574c1
narutoalai <31005849+narutolai@users.noreply.github.com>
2021-04-06 17:00:40 +0800

    Create 680 验证回文字符串.cpp

---
+++ 202104/680 验证回文字符串.cpp
@@ -0,0 +1,42 @@
+给定一个字符串，你只可以删除一次一个字符，然后看起是不是回文串。
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int i=0,j=s.size()-1;
+        int time=1;
+        while(i<=j)
+        {
+            if(s[i]==s[j])
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                j--;
+                if(time==0)break;
+                time--;
+            }
+        }
+        if(i>j) return true;
+        i=0;
+        j=s.size()-1;
+        time=1;
+         while(i<=j)
+        {
+            if(s[i]==s[j])
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                i++;
+            if(time==0) break;
+                 time--;   
+            }
+        }
+        if(i>j) return true;
+        return false;
+    }
+};
