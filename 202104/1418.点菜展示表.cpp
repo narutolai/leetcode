d70f6294b8cf3e453fd265148cf473ae2947da04
narutoalai <31005849+narutolai@users.noreply.github.com>
2021-07-07 14:08:29 +0800

    Update 1418.点菜展示表.cpp

--- 202104/1418.点菜展示表.cpp
+++ 202104/1418.点菜展示表.cpp
@@ -122,3 +122,17 @@ public:
         return result;
     }
 };
+int main()
+{
+    Solution so;
+    vector<vector<string>> order = {
+        {"David", "3", "Ceviche"},
+        {"Corina", "10", "Beef Burrito"},
+        {"David", "3", "Fried Chicken"},
+        {"Carla", "5", "Water"},
+        {"Carla", "5", "Ceviche"},
+        {"Rous", "3", "Ceviche"}};
+    so.displayTable(order);
+
+    return 0;
+}
