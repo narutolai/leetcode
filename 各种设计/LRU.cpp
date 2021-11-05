e92f50e7b7858ecbf35ab95a6015e3ab4a3be246
narutoalai <31005849+narutolai@users.noreply.github.com>
2021-11-05 15:37:39 +0800

    Create LRU.cpp

---
+++ 各种设计/LRU.cpp
@@ -0,0 +1,219 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+//LRU算法 数据结构的核心是哈希链表
+struct ListNode
+{
+    int key, val;
+    ListNode *next;
+    ListNode *prev;
+    ListNode() : key(0), val(0), next(nullptr), prev(nullptr) {}
+    ListNode(int k, int v) : key(k), val(v), next(nullptr), prev(nullptr) {}
+    ~ListNode()
+    {
+        cout << "delete ListNode" << endl;
+    }
+};
+typedef struct ListNode ListNode;
+
+//双端链表
+class DoubleList
+{   
+public:
+    DoubleList() : mSize(0), mHead(new ListNode()), mTail(new ListNode())
+    {
+        mHead->prev = nullptr;
+        mHead->next = mTail;
+        mTail->prev = mHead;
+        mTail->next = nullptr;
+    }
+    //将一个节点添加到链表头
+    void addFirst(ListNode *x)
+    {
+        //首个元素添加
+        if (mHead->next == mTail)
+        {
+            mHead->next = x;
+            x->next = mTail;
+
+            mTail->prev = x;
+            x->prev = mHead;
+        }
+        else
+        {
+            ListNode *fisrt = mHead->next;
+            mHead->next = x;
+            x->next = fisrt;
+
+            fisrt->prev = x;
+            x->prev = mHead;
+        }
+        //节点个数加1
+        mSize++;
+    }
+    //这么写太危险了吧
+    void remove(ListNode *x)
+    {
+        x->prev->next = x->next;
+        x->next->prev = x->prev;
+
+        x->next = nullptr;
+        x->prev = nullptr;
+        mSize--;
+    }
+    //返回移除的节点 如果移除失败 返回nullptr
+    ListNode *removeLast()
+    {
+        if (mTail->prev != mHead)
+        {
+            ListNode *last = mTail->prev;
+            last->prev->next = mTail;
+            mTail->prev = last->prev;
+
+            last->next = nullptr;
+            last->prev = nullptr;
+            mSize--;
+            return last;
+        }
+        return nullptr;
+    }
+    int size() { return mSize; }
+    void ShowInfo()
+    {
+        ListNode *pCur = mHead->next;
+        for (; pCur != mTail; pCur = pCur->next)
+        {
+            cout << pCur->key << " " << pCur->val << " ";
+        }
+        cout << endl;
+    }
+    ~DoubleList()
+    {
+        cout << "delete head and tail" << endl;
+        delete mHead;
+        delete mTail;
+    }
+
+private:
+    ListNode *mHead; //表头指针。
+    ListNode *mTail; //表尾指针
+    int mSize;       //当前链表中的节点个数
+};
+//LRUCache
+class LRUCache
+{
+private:
+    map<int, ListNode *> mKey2NodeMap;
+    DoubleList mCache;
+    int mCap;
+    bool full()
+    {
+        return mCap == mCache.size();
+    }
+
+public:
+    LRUCache(int capacity) : mCap(capacity) {}
+    ~LRUCache()
+    {
+        cout << "delete cachenode" << endl;
+        for (auto iter = mKey2NodeMap.begin(); iter != mKey2NodeMap.end(); iter++)
+        {
+            delete iter->second;
+        }
+    }
+
+    //根据key值找到节点 用于Cache  什么是访问 读取是访问 写回也是访问。
+    int read(int key)
+    {
+        //没有该节点
+        if (mKey2NodeMap.count(key) != 1)
+            return -1;
+        //获取改节点
+        ListNode *pNode = mKey2NodeMap[key];
+        mCache.remove(pNode);
+        mCache.addFirst(pNode);
+        return pNode->val;
+    }
+
+    //最近使用过的要放在最前边
+    void write(int key, int value)
+    {
+        //没有该节点
+        if (mKey2NodeMap.count(key) != 1)
+        {
+            if (full())
+            {
+                cout << "Capicity is full" << endl;
+                ListNode *pLastNode = mCache.removeLast();
+                mKey2NodeMap.erase(pLastNode->key);
+               
+            }
+            //新造一个节点
+            ListNode *pNode = new ListNode(key, value);
+            mCache.addFirst(pNode);
+            mKey2NodeMap[key] = pNode;
+        }
+        else
+        {
+            ListNode *pNode = mKey2NodeMap[key];
+            pNode->val = value;
+            mCache.remove(pNode);
+            mCache.addFirst(pNode);
+        }
+    }
+    void ShowLog()
+    {
+
+        for (auto item : mKey2NodeMap)
+        {
+            cout << item.first << " ";
+        }
+        cout << endl;
+        mCache.ShowInfo();
+    }
+};
+
+int main()
+{
+    {
+        LRUCache LruCache(4);
+
+        LruCache.write(1, 10);
+        LruCache.ShowLog();
+
+        LruCache.write(2, 20);
+        LruCache.ShowLog();
+
+        LruCache.write(3, 50);
+        LruCache.ShowLog();
+
+        LruCache.write(4, 30);
+        LruCache.ShowLog();
+
+        LruCache.write(6, 60);
+        LruCache.ShowLog();
+    }
+
+    return 0;
+}
+
+//LFU 算法
+
+//twwiter设计
+
+//智能指针设计 RAII 设计
+
+//设计模式懒汉饿汉单例模式或其他模式
+
+//简单进程池
+
+//简单线程池
+
+//简单内存池
+
+//并发版本！！
+
+//模板元编程
+
+//负载均衡算法
